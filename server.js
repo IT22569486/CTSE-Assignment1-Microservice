f24ee7c75f9d1494ce297000d8137e1cc66bbac3
Jayaisuru Samarakoon <128087293+IT22569486@users.noreply.github.com>
2026-05-01 20:31:02 +0530

    Update CORS origin to support multiple localhost addresses

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173'||'http://127.0.0.1:5173',
     credentials: true,
   })
 );
