85483bc90cf8c3647395f42ac0197f71a549171f
Jayaisuru Samarakoon <128087293+IT22569486@users.noreply.github.com>
2026-05-01 22:00:13 +0530

    cdc
    dcec

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -10,25 +10,9 @@ const { protect, adminOnly } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // POST /payments
-// router.post(
-//   '/',
-//   protect,
-//   [
-//     body('orderId').trim().notEmpty().withMessage('orderId is required'),
-//     body('userId').trim().notEmpty().withMessage('userId is required'),
-//     body('amount').isFloat({ gt: 0 }).withMessage('amount must be greater than 0'),
-//     body('currency').optional().isLength({ min: 3, max: 3 }).withMessage('currency must be 3 letters'),
-//     body('paymentMethod')
-//       .optional()
-//       .isIn(['card', 'cash_on_delivery', 'bank_transfer'])
-//       .withMessage('Invalid payment method'),
-//   ],
-//   processPayment
-// );
-
 router.post(
   '/',
-  // protect,
+  protect,
   [
     body('orderId').trim().notEmpty().withMessage('orderId is required'),
     body('userId').trim().notEmpty().withMessage('userId is required'),
@@ -43,24 +27,17 @@ router.post(
 );
 
 // GET /payments/:userId
-// router.get(
-//   '/:userId',
-//   protect,
-//   [param('userId').trim().notEmpty().withMessage('userId is required')],
-//   getPaymentsByUser
-// );
-
 router.get(
   '/:userId',
-  // protect,
-  // [param('userId').trim().notEmpty().withMessage('userId is required')],
+  protect,
+  [param('userId').trim().notEmpty().withMessage('userId is required')],
   getPaymentsByUser
 );
 
-// GET /payments (admin only)
-// router.get('/', protect, adminOnly, getAllPayments);
 
-router.get('/', getAllPayments);
+// GET /payments (admin only)
+router.get('/', protect, adminOnly, getAllPayments);
+
 
 
 module.exports = router;
